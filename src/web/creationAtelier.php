7b7ac7460a7d6a0852ceea14520a88717bb2604d
qramas <quentinusradus@hotmail.fr>
2016-10-06 15:31:08 +0200

    feat : link create atelier php and html

--- src/web/creationAtelier.php
+++ src/web/creationAtelier.php
@@ -5,26 +5,32 @@ Permet la creation d'un atelier en rentrant les différentes informations
 parametre example: "nom" : Presentation de Scrum "theme": conduite de projet, etc...
 */
 include 'config.php';
-$id= 1;
-$nom = "test";
-$theme = "test";
-$type = "test";
-$discipline = "test";
+
+echo("<head>");
+echo("<title>Ajout d'Atelier</title>");
+echo("<meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>");
+echo("</head>");
+
+
+$nom = $_POST['titre'];
+$theme = $_POST['theme'];
+$type = $_POST['type'];
+$discipline = $_POST['disciplines'];
 $public = "00110001";
 $duree = 1;
 $capacite=20;
-$animateur ="Quentin, Valentin, Berenice";
-$adresse= "test";
-$ville= "test";
+$animateur =$_POST['laboratoire'];
+$adresse= $_POST['adresse'];
+$ville= $_POST['ville'];
 $codeP =33600;
-$zone ="A";
+$zone =$_POST['zone'];
 $creneaux = "01230";
-$resume = "test";
+$resume = $_POST['resume'];
 
 $sql = "INSERT INTO `cdpm2`.`ateliers` (`id`, `nom`, `theme`, `type`, `discipline`, `public`, `duree`, 
 										`capacite`,`animateur`,`adresse`, `ville`, `codePostal`, `zone`, 
 										`creneaux`, `resume`) 
-		VALUES ('$id', '$nom', '$theme', '$type', '$discipline', '$public','$duree','$capacite',
+		VALUES (NULL, '$nom', '$theme', '$type', '$discipline', '$public','$duree','$capacite',
 				'$animateur','$adresse','$ville','$codeP','$zone','$creneaux','$resume');";
 		if ($res=$conn->query($sql)) {
 			echo "Création réussi";
